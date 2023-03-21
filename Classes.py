e66ff31e2bd9fdf6cc3d9cf0b06f47b8a02baa19
YuliaCheprasova <cheprasovaja@yandex.ru>
2023-03-21 20:19:21 +0700

    Initial commit

---
+++ Classes.py
@@ -0,0 +1,59 @@
+import datetime
+
+
+class wish():
+    """The class that generates wish of a person through dialogue"""
+
+    def subject(self) -> str:
+        """The method that determines a subject of sending"""
+
+        print('Write who you want to send on a journey, for example, hateful man')
+        who = input()
+        return who
+
+    def place(self, who: str) -> str:
+        """The method that determines a place of sending"""
+
+        print('I want to send a ', who, ' to (write any place in the world as the sending place, for example, hell) ')
+        where = input()
+        return where
+
+    def time(self, who: str, where: str) -> int:
+        """The method that determines the date of sending
+        and calculates the difference between it and the present time"""
+
+        print(
+            'I want to send a ', who, ' to ', where, ' in '
+            '(write the sending date you want in the year-month-date format, for example, 2028-06-09) ')
+        when = input()
+        when = datetime.datetime.strptime(when, '%Y-%m-%d')
+        when = when.date()
+        now = datetime.datetime.now()
+        now = now.date()
+        tdelta = when - now
+        tdelta = tdelta.days
+        return tdelta
+
+
+class time_machine():
+    """The class that simulates actions of the time machine"""
+
+    def sending(self, mas):
+        """The method that generates the sending message"""
+
+        if mas[2] >= 0:
+            print('I am sending a ', mas[0], ' to ', mas[1], ' ', str(mas[2]), ' days forward')
+        else:
+            print('I am sending a ', mas[0], ' to ', mas[1], ' ', str(mas[2]), ' days ago')
+
+
+
+
+"""def __main__():
+    a = wish()
+    b = time_machine()
+    who = a.subject()
+    where = a.place(who)
+    when = a.time(who, where)
+    b.request(who, where, when)
+    b.result()"""
---
+++ main.py
@@ -0,0 +1,48 @@
+import random
+import threading
+import queue
+import time
+from datetime import datetime
+from Classes import wish, time_machine
+
+
+request_history=[]
+flag=1
+
+def request (q):
+    r = wish()
+    global flag
+    while(flag==1):
+        who = r.subject()
+        where = r.place(who)
+        when = r.time(who, where)
+        new_item=[who,where,when,datetime.now().strftime('%H:%M:%S')]
+        global request_history
+        request_history+=[new_item]
+        q.put(new_item)
+        time.sleep(random.randint(2,5))
+
+def answer(q):
+    a = time_machine()
+    while(True):
+        request=q.get()
+        a.sending(request)
+
+
+q=queue.Queue()
+thr1=threading.Thread(target=request, args=(q,), daemon=True)
+thr1.start()
+thr2 = threading.Thread(target=answer, args=(q,), daemon=True)
+thr2.start()
+time.sleep(30)
+flag=0
+while(True):
+    if not thr1.is_alive():
+        break
+
+print("\nRequests history:")
+for i in range(len(request_history)):
+    if request_history[i][2]>=0:
+        print(f"{i+1}. A {request_history[i][0]} was sent to {request_history[i][1]} {request_history[i][2]} days forward at {request_history[i][3]}")
+    else:
+        print(f"{i+1}. A {request_history[i][0]} was sent to {request_history[i][1]} {request_history[i][2]} days ago at {request_history[i][3]}")
